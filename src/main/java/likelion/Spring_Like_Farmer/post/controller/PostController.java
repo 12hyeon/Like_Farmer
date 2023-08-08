cc621259a6f6a467c5de35cae7ad4cc42d4d71e7
JiSoo-Yang <yangjs229@gmail.com>
2023-08-08 20:39:47 +0900

    post logic

--- src/main/java/likelion/Spring_Like_Farmer/post/controller/PostController.java
+++ src/main/java/likelion/Spring_Like_Farmer/post/controller/PostController.java
@@ -11,12 +11,17 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
     private final PostService postService;
 
-    // Create a new post
+    // 게시글 올리기
     @PostMapping("/create")
     public Long createPost(@RequestBody PostDto.CreatePost request) {
         return postService.createPost(request);
     }
-    // Delete a post
+    // 게시글 수정하기
+    @PatchMapping("/update/{postId}")
+    public void updatePost(@PathVariable Long postId, @RequestBody PostDto.UpdatePost request) {
+        postService.updatePost(postId, request);
+    }
+    // 게시글 삭제하기
     @DeleteMapping("/delete/{postId}")
     public void deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
