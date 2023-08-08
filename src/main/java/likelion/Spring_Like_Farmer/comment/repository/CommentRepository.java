805dcee7f127bb5c36f42048b403f73fa59d90d3
JiSoo-Yang <130431316+JiSoo-Yang@users.noreply.github.com>
2023-08-08 20:58:36 +0900

    Delete src/main/java/likelion/Spring_Like_Farmer/comment directory

--- src/main/java/likelion/Spring_Like_Farmer/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package likelion.Spring_Like_Farmer.comment.repository;
-
-import likelion.Spring_Like_Farmer.comment.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
